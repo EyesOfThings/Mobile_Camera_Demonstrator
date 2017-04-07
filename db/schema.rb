6a1354f03f0c82c71f803e6b52935a001ed49c70
Junaid Farooq <junaid@evercam.io>
2017-04-07 14:56:10 +0500

    Switch to PG.

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20170407091447) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "users", force: :cascade do |t|
     t.string   "provider"
     t.string   "uid"
