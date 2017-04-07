73e119d37ac45608e10735d01199cdc1bbecd6fe
Junaid Farooq <junaid@evercam.io>
2017-04-07 14:15:47 +0500

    Added home and sessions controller.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  get 'sessions/create'
+
+  get 'sessions/destroy'
+
+  get 'home/show'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
