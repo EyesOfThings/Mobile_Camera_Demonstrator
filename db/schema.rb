0a760d619865ae0c9d5d647d9f7e7009aad2ace9
Junaid Farooq <junaid@evercam.io>
2017-04-13 17:12:28 +0500

    Image profile added to User.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170407091447) do
+ActiveRecord::Schema.define(version: 20170413115829) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20170407091447) do
     t.datetime "oauth_expires_at"
     t.datetime "created_at",       null: false
     t.datetime "updated_at",       null: false
+    t.string   "image"
   end
 
 end
