330678a430cf454d1fb4996d2282fd6ccf540d68
Junaid Farooq <junaid@evercam.io>
2017-04-27 12:59:46 +0500

    Changed File structure same as Evercam.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -16,14 +16,23 @@ class HomeController < ApplicationController
   end
 
   def send_to_seaweedfs
+    date = params[:timestamp].to_i
+    year = Time.at(date).utc.strftime("%Y")
+    month = Time.at(date).utc.strftime("%m")
+    day = Time.at(date).utc.strftime("%d")
+    hour = Time.at(date).utc.strftime("%H")
+    minutes = Time.at(date).utc.strftime("%M")
+    seconds = Time.at(date).utc.strftime("%S")
+
+    file_name = "#{minutes}_#{seconds}_000.jpg"
     begin
-      open("#{params[:timestamp]}.jpg", 'wb') do |file|
+      open(file_name, 'wb') do |file|
         file << open(params[:url]).read
       end
 
-      RestClient.post("#{ENV['seaweedFiler']}/#{params[:dir_name]}/snapshots/recordings/",
-        :name_of_file_param => File.new("#{params[:timestamp]}.jpg"))
-      File.delete("#{params[:timestamp]}.jpg")
+      RestClient.post("#{ENV['seaweedFiler']}/#{params[:dir_name]}/snapshots/recordings/#{year}/#{month}/#{day}/#{hour}/",
+        :name_of_file_param => File.new(file_name))
+      File.delete(file_name)
       render json: "1"
     rescue Exception => e
       render json: e.to_json
